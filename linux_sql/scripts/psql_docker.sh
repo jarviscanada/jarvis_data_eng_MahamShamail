2b4b0cf7a8d73f341d01a664338c87150777486b
Maham Shamail <mahamshamail@gmail.com>
2021-05-10 22:13:03 +0000

    update

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -7,6 +7,7 @@ sudo systemctl status docker || systemctl start docker
 docker pull postgres
 
 #save command line arguments as variables
+container_name="jrvs-psql"
 user_action_arg=$1 # create | start | stop
 db_username=$2
 db_password=$3
@@ -15,7 +16,7 @@ db_password=$3
 case "$user_action_arg" in
   #create case
   "create")
-    echo "Trying to create jrvs-psql container..."
+    echo "Trying to create $container_name container..."
       #check the number of arguments and see if username and password arguments are missing
       if  [ "$#" -ne 3 ] || [ "$db_username" == "" ] || [ "$db_password" == "" ];
       then
@@ -23,26 +24,26 @@ case "$user_action_arg" in
           exit 1
       fi
 
-      #check if 'jrvs-psql' container is already created, if so exit with 1
+      #check if container is already created, if so exit with 1
       container_is_created='docker ls -a -f name=jrvs-psql | wc -l'; #-f is to filter, wc -l prints length
       if [ "$container_is_created" -eq 2 ];
       then
-        echo "ERROR: Container jrvs-psql already exists. Exiting program."
+        echo "ERROR: Container $container_name already exists. Exiting program."
         exit 1
       fi
 
       #create a new docker volume
       docker volume create pgdata
 
-      #create a container using psql image with name=jrvs-psql
-      docker run --name jrvs-psql -e POSTGRES_PASSWORD="$db_password" -e POSTGRES_USER="$db_username" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+      #create a container using psql image
+      docker run --name "$container_name" -e POSTGRES_PASSWORD="$db_password" -e POSTGRES_USER="$db_username" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
        #What's $?
       exit $?
   ;;
 
   #start case
   "start")
-    echo "Trying to start jrvs-psql container..."
+    echo "Trying to start $container_name container..."
     #check number of arguments
     if [ "$#" -ne 1 ];
     then
@@ -58,14 +59,14 @@ case "$user_action_arg" in
       exit 1
     fi
 
-    #start docker container jrvs-psql
-    docker container start jrvs-psql
+    #start docker container
+    docker container start "$container_name"
     echo "Docker container initialised successfully! :D"
   ;;
 
   #stop case
   "stop")
-    echo "Trying to stop jrvs-psql container..."
+    echo "Trying to stop $container_name container..."
     #check number of arguments
     if [ "$#" -ne 1 ];
     then
@@ -77,13 +78,13 @@ case "$user_action_arg" in
     container_is_created='docker ls -a -f name=jrvs-psql | wc -l';
     if [ "$container_is_created" -ne 2 ];
     then
-      echo "ERROR: Container jrvs-psql does not exist! Exiting script. :("
+      echo "ERROR: Container $container_name does not exist! Exiting script. :("
       exit 1
     fi
 
     # stop docker container
-    docker container stop jrvs-psql
-    echo "Docker container jrvs-psql stopped successfully."
+    docker container stop "$container_name"
+    echo "Docker container $container_name stopped successfully."
   ;;
   *)
     #invalid argument
